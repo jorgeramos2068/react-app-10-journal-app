69341288ee522712efbe2fef23cfd1e08c5e8afe
Jorge Ramos <jorgeramos2068@gmail.com>
2020-09-09 18:16:37 -0500

    Keep authentication state

--- src/types/types.js
+++ src/types/types.js
@@ -2,7 +2,9 @@ const types = {
   login: '[Auth] Login',
   logout: '[Auth] Logout',
   uiSetError: '[UI] Set error',
-  uiRemoveError: '[UI] Remove error'
+  uiRemoveError: '[UI] Remove error',
+  uiStartLoading: '[UI] Start loading',
+  uiFinishLoading: '[UI] Finish loading'
 }
 
 export default types;

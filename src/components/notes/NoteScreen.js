d870729ba889f42d2ae3764966ea197e0f67a098
Jorge Ramos <jorgeramos2068@gmail.com>
2020-09-14 10:28:43 -0500

    Add functionality to save notes in Firestore

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { activateNote } from '../../actions/notes';
 import useForm from '../../hooks/useForm';
 import NotesAppBar from './NotesAppBar';
 
@@ -8,6 +9,7 @@ const NoteScreen = () => {
   const [formValues, handleInputChange, reset] = useForm(note);
   const {body, title} = formValues;
   const activeId = useRef(note.id);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (note.id !== activeId.current) {
@@ -15,8 +17,10 @@ const NoteScreen = () => {
       activeId.current = note.id;
     }
   }, [note, reset]);
-  
-  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(activateNote(formValues.id, {...formValues}));
+  }, [formValues, dispatch]);
 
   return (
     <div className="notes__main-content">
@@ -27,12 +31,14 @@ const NoteScreen = () => {
           placeholder="Some awesome title"
           className="notes__title-input"
           autoComplete="off"
+          name="title"
           value={title}
           onChange={handleInputChange}
         />
         <textarea
           placeholder="What happened today"
           className="notes__textarea"
+          name="body"
           value={body}
           onChange={handleInputChange}
         >

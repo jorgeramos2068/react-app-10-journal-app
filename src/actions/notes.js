7830324beb38ff6875812ad858736eb59dfe9724
Jorge Ramos <jorgeramos2068@gmail.com>
2020-09-14 10:28:43 -0500

    Add functionality to save notes in Firestore

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,3 +1,4 @@
+import Swal from 'sweetalert2';
 import { db } from '../firebase/firebase-config';
 import loadNotes from '../helpers/loadNotes';
 import types from '../types/types';
@@ -33,4 +34,29 @@ export const startLoadingNotes = (uid) => {
 export const setNotes = (notes) => ({
   type: types.notesLoad,
   payload: notes
+});
+
+export const startSaveNote = (note) => {
+  return async (dispatch, getState) => {
+    const {uid} = getState().auth;
+    if (!note.url) {
+      delete note.url;
+    }
+    const noteToFirestore = {...note};
+    delete noteToFirestore.id;
+    await db.doc(`${uid}/journal/notes/${note.id}`).update(noteToFirestore);
+    dispatch(refreshNote(note.id, note));
+    Swal.fire('Saved', 'The note was saved successfully', 'success');
+  };
+};
+
+export const refreshNote = (id, note) => ({
+  type: types.notesUpdate,
+  payload: {
+    id,
+    note: {
+      id,
+      ...note
+    }
+  }
 });

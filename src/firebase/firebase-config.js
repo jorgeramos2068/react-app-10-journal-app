c4a43e1c2998e39cd547c6f9fb140f2f7226feba
Jorge Ramos <jorgeramos2068@gmail.com>
2020-10-04 17:38:01 -0500

    Configure testing environment and perform first tests

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -12,8 +12,25 @@ const firebaseConfig = {
   messagingSenderId: "1096150811017",
   appId: "1:1096150811017:web:e5fc20dd62af99aab9e158"
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+
+const firebaseConfigTesting = {
+  apiKey: "AIzaSyA1OKs-nQHeMIT_8e4LTeQPgzvgcqjRPp8",
+  authDomain: "react-journal-app-jramos-test.firebaseapp.com",
+  databaseURL: "https://react-journal-app-jramos-test.firebaseio.com",
+  projectId: "react-journal-app-jramos-test",
+  storageBucket: "react-journal-app-jramos-test.appspot.com",
+  messagingSenderId: "901109002421",
+  appId: "1:901109002421:web:fcaa8468695a2e8f128efc"
+};
+
+if (process.env.NODE_ENV === 'test') {
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfigTesting);
+}
+else {
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+}
 
 // Reference to the DB
 const db = firebase.firestore();
---
+++ src/tests/actions/ui.test.js
@@ -0,0 +1,34 @@
+import ui, { setError } from '../../actions/ui';
+import types from '../../types/types';
+
+describe('Tests for ui actions', () => {
+  test('Should work for setError action', () => {
+    const err = 'error_type';
+    const action = setError(err);
+    expect(action).toEqual({
+      type: types.uiSetError,
+      payload: err
+    });
+  });
+
+  test('Should work for removeError action', () => {
+    const action = removeError();
+    expect(action).toEqual({
+      type: types.uiRemoveError
+    });
+  });
+
+  test('Should work for startLoading action', () => {
+    const action = startLoading();
+    expect(action).toEqual({
+      type: types.uiStartLoading
+    });
+  });
+
+  test('Should work for finishLoading action', () => {
+    const action = finishLoading();
+    expect(action).toEqual({
+      type: types.uiFinishLoading
+    });
+  });
+});

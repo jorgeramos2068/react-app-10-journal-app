1320769c6c7e2661cbbc1efe0b723f17ca8866f6
Jorge Ramos <jorgeramos2068@gmail.com>
2020-10-05 10:22:35 -0500

    Add enviroment variables

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -4,33 +4,17 @@ import 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAzoPtdJrciK2Ajt9WXeSSnBKR4I21Acjk",
-  authDomain: "react-journal-app-jramos.firebaseapp.com",
-  databaseURL: "https://react-journal-app-jramos.firebaseio.com",
-  projectId: "react-journal-app-jramos",
-  storageBucket: "react-journal-app-jramos.appspot.com",
-  messagingSenderId: "1096150811017",
-  appId: "1:1096150811017:web:e5fc20dd62af99aab9e158"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-const firebaseConfigTesting = {
-  apiKey: "AIzaSyA1OKs-nQHeMIT_8e4LTeQPgzvgcqjRPp8",
-  authDomain: "react-journal-app-jramos-test.firebaseapp.com",
-  databaseURL: "https://react-journal-app-jramos-test.firebaseio.com",
-  projectId: "react-journal-app-jramos-test",
-  storageBucket: "react-journal-app-jramos-test.appspot.com",
-  messagingSenderId: "901109002421",
-  appId: "1:901109002421:web:fcaa8468695a2e8f128efc"
-};
-
-if (process.env.NODE_ENV === 'test') {
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfigTesting);
-}
-else {
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 // Reference to the DB
 const db = firebase.firestore();

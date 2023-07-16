e29a99a95d5ef7fa3f49224e87d3de26a0c808e4
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-16 15:58:56 -0600

    Install and configure Redux

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,23 +1,48 @@
 import React from 'react';
+import {useDispatch} from 'react-redux';
 import {Link} from 'react-router-dom'
+import useForm from '../../hooks/useForm';
+import {startLoginEmailPassword, startGoogleLogin} from '../../actions/auth';
 
 const LoginScreen = () => {
+  const dispatch = useDispatch();
+
+  const[formValues, handleInputChange] = useForm({
+    email: 'batman@gmail.com',
+    password: 'password'
+  });
+
+  const {email, password} = formValues;
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    dispatch(startLoginEmailPassword(email, password));
+  };
+
+  const hangleGoogleLogin = () => {
+    dispatch(startGoogleLogin());
+  };
+
   return (
     <>
       <h3 className="auth__title">Login</h3>
-      <form>
+      <form onSubmit={handleLogin}>
         <input
           type="text"
           placeholder="Email"
           name="email"
           className="auth__input"
           autoComplete="off"
+          value={email}
+          onChange={handleInputChange}
         />
         <input
           type="password"
           placeholder="Password"
           name="password"
           className="auth__input"
+          value={password}
+          onChange={handleInputChange}
         />
         <button
           type="submit"
@@ -29,6 +54,7 @@ const LoginScreen = () => {
           <p>Login with Social Media</p>
           <div 
             className="google-btn"
+            onClick={hangleGoogleLogin}
           >
             <div className="google-icon-wrapper">
               <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
